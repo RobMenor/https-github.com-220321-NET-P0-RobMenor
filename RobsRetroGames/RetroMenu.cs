620043148cc5220f23d26d16848ea0789b88d6ef
Rob Menor <robert927@revature.net>
2022-04-03 11:54:17 -0700

    Modified the RetroMenu.cs file

--- RobsRetroGames/RetroMenu.cs
+++ RobsRetroGames/RetroMenu.cs
@@ -7,35 +7,93 @@ public class RetroMenu
     {
         Console.WriteLine("Welcome to Rob's Retro Games.");
 
-        EnterBrand:
-        Console.WriteLine("What console are you looking for today?");
-        
-        string? brand = Console.ReadLine();
+        Main:
+        Console.WriteLine("Have you shopped with us before? [Y/N]");
 
-        if(String.IsNullOrWhiteSpace(brand))
-        {
-            Console.WriteLine("Please enter valid brand.");
-            goto EnterBrand;
+        string newShopper = Console.ReadLine() ?? "";
+
+        char respChar;
+            try
+            {
+                respChar = newShopper.Trim().ToUpper()[0];
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Invalid response");
+                goto Main;
+            }
+
+            if(respChar == 'N')
+            {
+                Name:
+                Console.WriteLine("Please sign up. What is your name?");
+                string name = Console.ReadLine() ?? "";
+
+            if(String.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Please enter valid name.");
+                goto Name;
+            }
+            else if (respChar != 'Y')
+            {
+                Console.WriteLine("Invalid response");
+            }
         }
-
-        Console.WriteLine("You said " + brand);
-
-        EnterTitle:
-        Console.WriteLine("Ok. What title are you looking for?");
-        string? title = Console.ReadLine();
-
-        if(String.IsNullOrWhiteSpace(title))
+        bool exit = false;
+        do
         {
-            Console.WriteLine("Please enter valid title.");
-            goto EnterTitle;
-        }
+            EnterBrand:
+            Console.WriteLine("What console are you looking for today?");
+            
+            string? brand = Console.ReadLine();
 
-        //Inventory newInventory = new Inventory();
-        // newInventory.Brand = brand;
-        // newInventory.Title = title;
-        // newInventory.Price = 0;
+            if(String.IsNullOrWhiteSpace(brand))
+            {
+                Console.WriteLine("Please enter valid brand.");
+                goto EnterBrand;
+            }
 
-        Inventory newInventory = new Inventory(brand, title);
-        Console.WriteLine(newInventory.ToString());
+            Console.WriteLine("You said " + brand);
+
+            EnterTitle:
+            Console.WriteLine("Ok. What title are you looking for?");
+            string? title = Console.ReadLine();
+
+            if(String.IsNullOrWhiteSpace(title))
+            {
+                Console.WriteLine("Please enter valid title.");
+                goto EnterTitle;
+            }
+
+            Inventory newInventory = new Inventory(brand, title);
+            Console.WriteLine(newInventory.ToString());
+
+            Another:
+            Console.WriteLine("Would you like to look for something else? [Y/N]");
+            string enterAnother = Console.ReadLine() ?? "";
+            
+            char responseChar;
+            try
+            {
+                responseChar = enterAnother.Trim().ToUpper()[0];
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Invalid response");
+                goto Another;
+            }
+
+            if(responseChar == 'N')
+            {
+                Console.WriteLine("Have a good day!");
+                exit = true;
+            }
+            else if (responseChar != 'Y')
+            {
+                Console.WriteLine("Invalid response");
+                goto Another;
+            }
+        }while(!exit);
     }
+        
 }
