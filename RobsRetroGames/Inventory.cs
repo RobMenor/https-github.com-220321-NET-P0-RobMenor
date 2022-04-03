07e550fa3e3d402120a9ea244e6042a8038f1dc1
Rob Menor <robert927@revature.net>
2022-04-03 01:23:36 -0700

    added Inventory.cs file and RetroMenu.cs file, modified Program.cs file

---
+++ RobsRetroGames/Inventory.cs
@@ -0,0 +1,10 @@
+namespace RobsRMenu;
+
+public class Inventory
+{
+    public string Brand { get; set; }
+
+    public string Title { get; set; }
+
+    public int Price { get; set; }
+}
---
+++ RobsRetroGames/RetroMenu.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RobsRMenu;
+
+public class RetroMenu
+{
+    public void MainMenu()
+    {
+        Console.WriteLine("Welcome to Rob's Retro Games.");
+        Console.WriteLine("What console are you looking for today?");
+        
+        string? brand = Console.ReadLine();
+        Console.WriteLine("You said " + brand);
+
+        Console.WriteLine("Ok. What title are you looking for?");
+        string? title = Console.ReadLine();
+        Console.WriteLine("Ok. Let me see if I have " + title + " for " + brand);
+    }
+}
