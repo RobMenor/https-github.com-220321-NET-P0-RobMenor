a87cd4d5aec34e4bd563d06fa66d28099e086993
Rob Menor <robert927@revature.net>
2022-04-03 03:37:31 -0700

    updated Inventory.cs file and RetroMenu.cs file

--- RobsRetroGames/Inventory.cs
+++ RobsRetroGames/Inventory.cs
@@ -2,9 +2,37 @@ namespace RobsRMenu;
 
 public class Inventory
 {
-    public string Brand { get; set; }
+    public Inventory()
+    {
+        Price = 0;
+        _brand = "";
+        Title = "";
+    }
 
-    public string Title { get; set; }
+    public Inventory(string brand, string title) : this()
+    {
+        _brand = brand;
+        Title = title;
+    }
+    private string? _brand;
+    public string? Brand
+    {
+        get
+        {
+            return _brand;
+        }
+        set
+        {
+            _brand = value;
+        }
+    }
+
+    public string? Title { get; set; }
 
     public int Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"Ok. Let me see if I have {this.Title} for {this.Brand} it'll be {this.Price} dollars";
+    }
 }
--- RobsRetroGames/RetroMenu.cs
+++ RobsRetroGames/RetroMenu.cs
@@ -6,13 +6,36 @@ public class RetroMenu
     public void MainMenu()
     {
         Console.WriteLine("Welcome to Rob's Retro Games.");
+
+        EnterBrand:
         Console.WriteLine("What console are you looking for today?");
         
         string? brand = Console.ReadLine();
+
+        if(String.IsNullOrWhiteSpace(brand))
+        {
+            Console.WriteLine("Please enter valid brand.");
+            goto EnterBrand;
+        }
+
         Console.WriteLine("You said " + brand);
 
+        EnterTitle:
         Console.WriteLine("Ok. What title are you looking for?");
         string? title = Console.ReadLine();
-        Console.WriteLine("Ok. Let me see if I have " + title + " for " + brand);
+
+        if(String.IsNullOrWhiteSpace(title))
+        {
+            Console.WriteLine("Please enter valid title.");
+            goto EnterTitle;
+        }
+
+        //Inventory newInventory = new Inventory();
+        // newInventory.Brand = brand;
+        // newInventory.Title = title;
+        // newInventory.Price = 0;
+
+        Inventory newInventory = new Inventory(brand, title);
+        Console.WriteLine(newInventory.ToString());
     }
 }
